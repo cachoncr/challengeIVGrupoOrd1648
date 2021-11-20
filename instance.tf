09c7814315499b01d63798498afd185474ca789d
cachoncr <rodriguez.ncr@gmail.com>
2021-11-20 19:17:53 -0300

    Entrega V3.0

--- instance.tf
+++ instance.tf
@@ -4,7 +4,7 @@ resource "aws_instance" "server1" {
   subnet_id                   = aws_subnet.subnet1.id
   associate_public_ip_address = true
   vpc_security_group_ids      = [aws_security_group.allow_http_ssh.id]
-  private_ip                  = "172.16.12.100"
+  private_ip                  = "172.16.12.10"
   key_name                    = var.key1
   user_data                   = file("user-data.sh")
 
