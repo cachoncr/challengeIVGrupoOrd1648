b93e7b9a08b6ccbf23ad015b38d26ca62e92e549
cachoncr <rodriguez.ncr@gmail.com>
2021-11-20 19:06:51 -0300

    Entre v2.0

--- networking.tf
+++ networking.tf
@@ -1,17 +1,17 @@
 resource "aws_subnet" "subnet1" {
   vpc_id                  = var.vpc1_id
-  cidr_block              = "192.168.10.28./16"
+  cidr_block              = "172.16.12.0/28"
   map_public_ip_on_launch = true
 
   tags = {
-    "Name" = "Subnet ORD1648"
+    "Name" = "Subnet Grupo 12"
   }
 }
 resource "aws_route_table" "r" {
   vpc_id = var.vpc1_id
   route {
     cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.gw.id
+    gateway_id = var.internet_gateway_id
   }
 }
 
